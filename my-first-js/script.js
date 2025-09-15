8db5ddd051bf9c86412d92a6d925cbe5db3593be
Estelle <emvmontiel@gmail.com>
2025-09-15 19:59:40 +0800

    smol progress on TOP

--- my-first-js/script.js
+++ my-first-js/script.js
@@ -1,1 +1,16 @@
 console.log("Hello, good evening!")
+
+let age = 2;
+console.log(age)
+
+let naniguwangUg = 5;
+console.log(age + naniguwangUg)
+
+let now = naniguwangUg + age;
+console.log(now)
+
+const pi = 3.14;
+console.log(pi)
+
+let trial = age + pi;
+console.log(trial )
