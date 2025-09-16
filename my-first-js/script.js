03266f1f9f85834632c8bbd7b7778f57af898fcc
Estelle <emvmontiel@gmail.com>
2025-09-16 22:03:01 +0800

    update

--- my-first-js/script.js
+++ my-first-js/script.js
@@ -13,4 +13,26 @@ const pi = 3.14;
 console.log(pi)
 
 let trial = age + pi;
-console.log(trial )
+console.log(trial)
+
+console.log((3 + 2) - 76 * (1 + 1))
+console.log(23 + 97)
+console.log(2 + 3 + 90 + 7 + 6 + 41 + 8)
+console.log((4 + 6 + 9) / 77 )
+
+let a = 10;
+console.log(a)
+
+a = 14;
+console.log(a)
+
+let b = 2 * a;
+console.log(b)
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(max)
+console.log(actual)
+console.log(percentage)
