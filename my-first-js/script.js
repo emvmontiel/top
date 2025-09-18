83cff7e50fc8da17197483e646ca59ba941e0142
Estelle <emvmontiel@gmail.com>
2025-09-18 21:18:01 +0800

    update: data types, strings, conditionals

--- my-first-js/script.js
+++ my-first-js/script.js
@@ -66,3 +66,31 @@ console.log(text)
 
 const text2 = `One day, I am going to be free\nfrom everything I see.`
 console.log(text2)
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+const dateToday = new Date();
+console.log(dateToday);
+// console.log(dateToday.getDay());
+console.log(day);
