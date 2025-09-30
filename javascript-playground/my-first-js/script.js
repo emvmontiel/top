3dca688b6fe2c43022018bf2f3b5573a3794d045
Estelle <emvmontiel@gmail.com>
2025-09-30 15:05:39 +0800

    organized javascript files

---
+++ javascript-playground/my-first-js/script.js
@@ -0,0 +1,112 @@
+console.log("Hello, good evening!")
+
+let age = 2;
+console.log(age);
+
+let naniguwangUg = 5;
+console.log(age + naniguwangUg);
+
+let now = naniguwangUg + age;
+console.log(now);
+
+const pi = 3.14;
+console.log(pi);
+
+let trial = age + pi;
+console.log(trial);
+
+console.log((3 + 2) - 76 * (1 + 1));
+console.log(23 + 97);
+console.log(2 + 3 + 90 + 7 + 6 + 41 + 8);
+console.log((4 + 6 + 9) / 77 );
+
+let a = 10;
+console.log(a);
+
+a = 14;
+console.log(a);
+
+let b = 2 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(max);
+console.log(actual);
+console.log(percentage);
+
+console.log(1 / 0); // Why is this?
+
+const string = "The revolution will not be televised!";
+console.log(string);
+
+const backtick = `Hello! Does this work?`;
+console.log(backtick);
+
+const name = "Chris";
+const greeting = `Hello, ${name}!`;
+console.log(greeting) // Amazing
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined);
+
+const song = "Fight Song by Rachel Platten";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${(score / highestScore) * 100}%.`
+console.log(output)
+
+const text = `One day, I am going to be free
+from everything I see.`
+console.log(text)
+
+const text2 = `One day, I am going to be free\nfrom everything I see.`
+console.log(text2)
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+const dateToday = new Date();
+console.log(dateToday);
+// console.log(dateToday.getDay());
+console.log(day);
+
+const animal = "Hanimungkal";
+
+function favoriteAnimal(ikaw) {
+  return 'My favorite animal is ' + ikaw;
+}
+console.log(favoriteAnimal(animal));
+
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+const secondTrial = myText.replace("I", "ooo")
+console.log(newString);
+console.log(secondTrial);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
