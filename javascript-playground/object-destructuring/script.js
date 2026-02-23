6c862fbb43e30a81b2c4d08e6cba0045e925b04f
emvmontiel <emvmontiel@gmail.com>
2026-02-23 17:07:00 +0800

    object restructuring challenge

---
+++ javascript-playground/object-destructuring/script.js
@@ -0,0 +1,24 @@
+const dreamHoliday = {
+    place: 'Boracay, Philippines',
+    thing: 'beaches and coral reefs',
+    activity: 'surfing',
+    accommodation: 'Henan Hotel',
+    companion: 'Henry and company'
+}
+
+const {place, thing, activity, accommodation, companion} = dreamHoliday
+
+console.log(`I would love to go to ${place} to visit the ${thing} and go ${activity}. I'd sleep at the ${accommodation} and hang out with ${companion} all day. :)`)
+
+/* 
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add 
+   extra properties or change the existing ones. 
+2. Destructure the object and use the individual 
+   variables to log out one or more sentences about 
+   your dream holiday. 
+   
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
